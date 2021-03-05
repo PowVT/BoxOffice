9f7a3071d23941b719faf9024c882a487b07e74d
PowVT <epowlowsky@yahoo.com>
2021-03-05 14:41:27 -0500

    Update erc721.sol

--- erc721.sol
+++ erc721.sol
@@ -1,22 +1,20 @@
 pragma solidity >=0.4.2 <0.9.0;
+//SPDX-License-Identifier: 0BSD
 
-/**
- * @title ERC721
- * @dev Smart contract ERC721 standard copied from ethereum.org
- */
+//@title ERC721
+//@dev Smart contract ERC721 standard copied from ethereum.org
 
-
-contract ERC721 {
+abstract contract ERC721 {
     
   event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
   
   event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
 
-  function balanceOf(address _owner) external view returns (uint256);
+  function balanceOf(address _owner) external view virtual returns (uint256){}
   
-  function ownerOf(uint256 _tokenId) external view returns (address);
+  function ownerOf(uint256 _tokenId) external view virtual returns (address){}
   
-  function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable virtual{}
   
-  function approve(address _approved, uint256 _tokenId) external payable;
+  function approve(address _approved, uint256 _tokenId) external payable virtual{}
 }
